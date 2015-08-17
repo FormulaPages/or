c8020b26e2ee31e65826d6fed0974b9fffbbd988
Peter Moresi <peter.moresi@gmail.com>
2015-08-16 21:52:40 -0700

    v1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # or
 NPM Package for spreadsheet compatible OR function
+
+    npm install formula-or
--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 (function (global, factory) {
-    if (typeof define === "function" && define.amd) {
-        define(["exports", "module"], factory);
-    } else if (typeof exports !== "undefined" && typeof module !== "undefined") {
-        factory(exports, module);
+    if (typeof define === 'function' && define.amd) {
+        define(['exports', 'module', 'formula-flatten'], factory);
+    } else if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
+        factory(exports, module, require('formula-flatten'));
     } else {
         var mod = {
             exports: {}
         };
-        factory(mod.exports, mod);
-        global.index = mod.exports;
+        factory(mod.exports, mod, global.FLATTEN);
+        global.OR = mod.exports;
     }
-})(this, function (exports, module) {
-    "use strict";
+})(this, function (exports, module, _formulaFlatten) {
+    'use strict';
 
     module.exports = OR;
 
+    function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+    var _FLATTEN = _interopRequireDefault(_formulaFlatten);
+
     function OR() {
         var result = true;
-        var list = fn.flatten(arguments);
+        var list = FLATTENt(arguments);
         for (var i = 0; i < list.length; i++) {
             result = result && arguments[i];
             if (result) return true;
